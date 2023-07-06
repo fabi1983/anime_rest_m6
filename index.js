c5813c627f98b500c3b192e6d1a30135d3d817d7
fabi1983 <faberic1983@gmail.com>
2023-07-05 22:32:36 -0400

    post anime ok

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require ("express")
 const app = express()
 const fs = require('fs/promises')
+const {v4: uuidv4 } = require('uuid')  
 
 const PORT = 3000
+
+// para usar req.body
+app.use(express.json())
 
 // leer animes
 app.get('/anime', async( req, res ) => {
     const lecturaArchivo = await fs.readFile('anime.json')
-    res.send(lecturaArchivo);
+    //res.send(lecturaArchivo)
+    const objetoArchivoOriginal = JSON.parse(lecturaArchivo)
+    
+    res.send(objetoArchivoOriginal);
 })
 
 // leer 1 anime
@@ -21,6 +28,24 @@ app.get('/anime/:id', async( req, res ) => {
     // console.log(animeOriginal)
     res.send(animeOriginal)
 })
+
+app.post('/anime', async( req, res ) => {
+    const lecturaArchivo = await fs.readFile('anime.json')
+    const objetoArchivoOriginal = JSON.parse(lecturaArchivo)
+    // console.log(objetoArchivoOriginal)
+
+    const datosAnimeBody = req.body;
+
+    const id = uuidv4().slice(0,4)
+
+    objetoArchivoOriginal[id] = datosAnimeBody;  
+
+    //console.log(objetoArchivoOriginal)
+    
+    await fs.writeFile('anime.json', JSON.stringify(objetoArchivoOriginal, null, 2))
+    res.send('Se ha guardado exitosamente el nuevo anime');
+})
+
 
 
 
